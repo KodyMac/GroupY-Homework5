1c2f49e4b3099c4d380ff58a2b2fa4f16cac668c
Kody McNamara <Kody McNamara@10.7.15.217>
2024-02-29 16:29:36 -0600

    All of those methods in PizzaOrder initialized

--- src/hw5/PizzaOrder.java
+++ src/hw5/PizzaOrder.java
@@ -5,8 +5,59 @@ import hw5.pizza.AbstractPizza;
 import hw5.pizza.CookingStyleType;
 import hw5.pizza.PizzaType;
 import hw5.pizza.Toppings;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import hw5.PizzaCookingFactory;
 
 public class PizzaOrder {
 
+	private PizzaCookingFactory pizzaFactory;
+	private ICookingStrategy cookingStrategy;
+	private List<AbstractPizza> pizzaOrderList;
+	
+	public PizzaOrder() {
+		pizzaFactory = new PizzaCookingFactory();
+		pizzaOrderList = new ArrayList<AbstractPizza>();
+	}
+
+	public void printListOfToppingsByPizzaOrderID(int orderID) {
+		
+	}
+
+	
+	public void printPizzaOrderCart(int orderID) {
+		
+	}
+
+	public AbstractPizza getPizzaByOrderID(int orderID) {
+		return null;
+	}
+	
+	public boolean addPizzaToCart(PizzaType pizzaType) {
+		return false;
+	}
+	
+	public boolean addNewToppingToPizza(int orderID, Toppings topping) {
+		return false;
+	}
+	
+	public boolean removeToppingFromPizza(int orderID, Toppings topping) {
+		return false;
+	}
+	
+	public boolean isThereAnyUncookedPizza() {
+		return false;
+	}
+	
+	public double checkout() throws Exception {
+		return 0.0;
+	}
+	
+	public boolean selectCookingStrategyByPizzaOrderID(int orderID, CookingStyleType cookingStrategyType) {
+		return false;
+	}
 }
+
+
