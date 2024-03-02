4d42c8bc4fc6459a98d5881ecf314dd919b7ed97
Kody McNamara <Kody McNamara@10.7.15.217>
2024-03-02 12:46:53 -0600

    ICookingStrategy abstract cook method and javadoc it

--- src/hw5/cookingStrategy/ICookingStrategy.java
+++ src/hw5/cookingStrategy/ICookingStrategy.java
@@ -3,5 +3,13 @@ package hw5.cookingStrategy;
 import hw5.pizza.AbstractPizza;
 
 public interface ICookingStrategy {
-
+	/**
+	 * sets the cookingPrice, cookingStrategy and updates the pizzaPrice 
+	 * of the pizza that is being cooked and passed as a parameter. 
+	 * There could only be one cooking strategy selected for a pizza. The 
+	 * cooking strategy changes the pizza price 
+	 * @param pizza
+	 * @return
+	 */
+	public boolean cook(AbstractPizza pizza);
 }
