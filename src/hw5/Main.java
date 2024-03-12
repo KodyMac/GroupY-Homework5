3409c57e27dd2996ca3ac097a1b5d6836aa809f2
Kody McNamara <Kody McNamara@10.7.13.233>
2024-03-12 10:59:50 -0500

    main, part 1

--- src/hw5/Main.java
+++ src/hw5/Main.java
@@ -1,10 +1,30 @@
 package hw5;
 
+import hw5.pizza.CookingStyleType;
+import hw5.pizza.PizzaType;
+import hw5.PizzaOrder;
+import hw5.pizza.Toppings;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PizzaOrder order = new PizzaOrder();  //instantiate pizza order
+		System.out.println("--------------------------------------------------------------\n");
+		System.out.println("Adding one of each type of pizza ~\n");
+		System.out.println(order.addPizzaToCart(PizzaType.VEGETARIAN));
+		System.out.println(order.addPizzaToCart(PizzaType.HAWAIIAN));
+		System.out.println(order.addPizzaToCart(PizzaType.SUPREME));
+		System.out.println(order.addPizzaToCart(PizzaType.MARGHERITA));
+		
+		System.out.println("--------------------------------------------------------------\n");
+		System.out.println("Printing Order Cart\n");
+		order.printPizzaOrderCart(0);
+		
+		System.out.println("--------------------------------------------------------------\n");
+		System.out.println("Getting pizza by OrderID ~\n");
+		System.out.println("ID 1:  " + order.getPizzaByOrderID(1));
+		System.out.println("ID 3:  " + order.getPizzaByOrderID(3));
+		
 	}
 
 }
